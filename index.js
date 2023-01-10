f48c8e095fd6bb63b3d6e356f2f108239059a979
Checkmate990 <108345290+Checkmate990@users.noreply.github.com>
2023-01-09 21:16:19 -0700

    Second lab done

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
-// Code your solution in this file!
+let  Headquarter = 42
+
+function distanceFromHqInBlocks(distance){
+    return Math.abs(distance-Headquarter)
+}
+
+function distanceFromHqInFeet(distance){
+   return distanceFromHqInBlocks(distance)*264
+    
+}
+
+function distanceTravelledInFeet(origin, end){
+    return Math.abs((origin-end)*264);
+}
+
+function calculatesFarePrice(start, destination){
+    let  feet = Math.abs (((start - destination)*264))
+    if(start === 43 && destination === 44){
+    return 0
+}  else if 
+    (feet >= 400 && feet <= 2000){
+    let price= (feet - 400) * 0.02
+    return price
+}  else if (feet>2000 && feet <= 2500){
+    return 25
+}   else{
+    return 'cannot travel that far'
+}
+}
+
+
+
+
+
+
